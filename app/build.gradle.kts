6eef52a41b8bbaa650eba5e030d262816544d07a
Petr Holub <petr.holub@bbmri-eric.eu>
2026-06-08 06:33:34 +0200

    Allow Termux SDK level overrides

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,14 +2,22 @@ plugins {
     id("com.android.application")
 }
 
+val localCompileSdk = providers.gradleProperty("termuxCompileSdk")
+    .map(String::toInt)
+    .getOrElse(36)
+
+val localTargetSdk = providers.gradleProperty("termuxTargetSdk")
+    .map(String::toInt)
+    .getOrElse(36)
+
 android {
     namespace = "org.rtkcollector.app"
-    compileSdk = 36
+    compileSdk = localCompileSdk
 
     defaultConfig {
         applicationId = "org.rtkcollector.app"
         minSdk = 26
-        targetSdk = 36
+        targetSdk = localTargetSdk
         versionCode = 1
         versionName = "0.1.0"
     }
